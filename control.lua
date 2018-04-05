40ad9c59880823fc76c8fbf36c64dba48c60d054
Shane Madden <shanemadden@fake.email>
2018-04-05 08:52:50 -0600

    Track reconnection targets that get destroyed by unit number instead of using find_entity()

--- control.lua
+++ control.lua
@@ -6,6 +6,7 @@ local function on_player_selected_area(event)
         local info = {
           surface = entity.surface,
           name = entity.name,
+          unit_number = entity.unit_number,
           position = entity.position,
           health = entity.health,
           force = entity.force,
@@ -16,15 +17,15 @@ local function on_player_selected_area(event)
             wire = connection.wire,
             source_circuit_id = connection.source_circuit_id,
             target_circuit_id = connection.target_circuit_id,
+            target_unit_number = connection.target_entity.unit_number,
             target_entity = connection.target_entity,
-            target_position = connection.target_entity.position,
-            target_name = connection.target_entity.name,
           }
         end
         table.insert(create, info)
         entity.destroy()
       end
     end
+    local old_new_map = {}
     for k, newinfo in pairs(create) do
       local newpole = newinfo.surface.create_entity({
         name = newinfo.name,
@@ -33,6 +34,7 @@ local function on_player_selected_area(event)
       })
       newpole.health = newinfo.health
       create[k].newpole = newpole
+      old_new_map[newinfo.unit_number] = newpole
     end
     for i, newinfo in pairs(create) do
       for k, connection in pairs(newinfo.connections) do
@@ -40,7 +42,7 @@ local function on_player_selected_area(event)
         if connection.target_entity.valid then
           target = connection.target_entity
         else
-          target = newinfo.surface.find_entity(connection.target_name, connection.target_position)
+          target = old_new_map[connection.target_unit_number]
         end
         if target then
           newinfo.newpole.connect_neighbour({
