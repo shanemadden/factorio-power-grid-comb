655e1cda3483a8dd279a2d0fbe69d592bb2085c4
Caleb Wishart <caleb.wish@outlook.com>
2024-11-01 20:36:03 +1000

    fix: Factorio 2.0 API Changes

--- control.lua
+++ control.lua
@@ -1,94 +1,41 @@
+-- Get the copper wire connector from an entity
+-- if it exists, return it, otherwise return nil
+local function get_copper_wire_connector(entity)
+  local connectors = entity.get_wire_connectors(false)
+  for _, connector in pairs(connectors) do
+    if connector.wire_type == defines.wire_type.copper then
+      return connector
+    end
+  end
+  return nil
+end
 
 local function on_player_selected_area(event)
-  if event.item ~= "power-grid-comb" then return end
-  local distances = {}
-  local create = {}
-  for k, entity in pairs(event.entities) do
-    if entity.name == "ret-pole-wire" then goto continue end
-    local info = {
-      surface = entity.surface,
-      name = entity.name,
-      unit_number = entity.unit_number,
-      position = entity.position,
-      health = entity.health,
-      force = entity.force,
-      connections = {},
-    }
-    for k, connection in pairs(entity.circuit_connection_definitions) do
-      info.connections[k] = {
-        wire = connection.wire,
-        source_circuit_id = connection.source_circuit_id,
-        target_circuit_id = connection.target_circuit_id,
-        target_unit_number = connection.target_entity.unit_number,
-        target_entity = connection.target_entity,
-      }
-    end
-    local max_wire_distance = entity.prototype.max_wire_distance
-    if create[max_wire_distance] == nil then
-      create[max_wire_distance] = {}
-      table.insert(distances, max_wire_distance)
-    end
-    table.insert(create[max_wire_distance], info)
-    entity.destroy()
-    ::continue::
-  end
-  table.sort(distances)
-  local old_new_map = {}
-  for _, distance in ipairs(distances) do
-    for k, newinfo in pairs(create[distance]) do
-      local newpole = newinfo.surface.create_entity({
-        name = newinfo.name,
-        position = newinfo.position,
-        force = newinfo.force,
-      })
-      newpole.health = newinfo.health
-      create[distance][k].newpole = newpole
-      old_new_map[newinfo.unit_number] = newpole
-    end
-  end
-  for _, distance in ipairs(distances) do
-    for _, newinfo in pairs(create[distance]) do
-      for k, connection in pairs(newinfo.connections) do
-        local target
-        if connection.target_entity.valid then
-          target = connection.target_entity
-        else
-          target = old_new_map[connection.target_unit_number]
-        end
-        if target then
-          newinfo.newpole.connect_neighbour({
-            wire = connection.wire,
-            target_entity = target,
-            source_circuit_id = connection.source_circuit_id,
-            target_circuit_id = connection.target_circuit_id,
-          })
-        end
-      end
-    end
-  end
-end
-script.on_event(defines.events.on_player_selected_area, on_player_selected_area)
-
-local function on_player_alt_selected_area(event)
   if event.item ~= "power-grid-comb" then return end
   local unit_numbers = {}
   local x_axis = {}
   local y_axis = {}
+
   for _, entity in pairs(event.entities) do
     if entity.name == "ret-pole-wire" then goto continue end
-
     unit_numbers[entity.unit_number] = true
+    ::continue::
+  end
+
+  for _, entity in pairs(event.entities) do
+    if entity.name == "ret-pole-wire" then goto continue end
     local to_reconnect = {}
-    local len = 0
-    for _, connection in pairs(entity.neighbours.copper) do
-      if not unit_numbers[connection.unit_number] then
-        len = len + 1
-        to_reconnect[len] = connection
+    local connector = get_copper_wire_connector(entity)
+    if connector == nil then goto continue end
+    for _, connection in pairs(connector.connections) do
+      --  if the connection is not in the selected area, we need to reconnect it
+      if not unit_numbers[connection.target.owner.unit_number] then
+        to_reconnect[#to_reconnect + 1] = connection.target
       end
     end
-    entity.disconnect_neighbour()
+    connector.disconnect_all()
     for _, connection in pairs(to_reconnect) do
-      entity.connect_neighbour(connection)
+      connector.connect_to(connection)
     end
 
     local position = entity.position
@@ -96,11 +43,11 @@ local function on_player_alt_selected_area(event)
 
     local x_entities = x_axis[x] or {}
     x_axis[x] = x_entities
-    x_entities[#x_entities+1] = entity
+    x_entities[#x_entities + 1] = entity
 
     local y_entities = y_axis[y] or {}
     y_axis[y] = y_entities
-    y_entities[#y_entities+1] = entity
+    y_entities[#y_entities + 1] = entity
 
     ::continue::
   end
@@ -110,7 +57,7 @@ local function on_player_alt_selected_area(event)
       return a.position.y < b.position.y
     end)
     for i = 2, #entities do
-      entities[i].connect_neighbour(entities[i-1])
+      get_copper_wire_connector(entities[i]).connect_to(get_copper_wire_connector(entities[i - 1]))
     end
   end
 
@@ -119,8 +66,8 @@ local function on_player_alt_selected_area(event)
       return a.position.x < b.position.x
     end)
     for i = 2, #entities do
-      entities[i].connect_neighbour(entities[i-1])
+      get_copper_wire_connector(entities[i]).connect_to(get_copper_wire_connector(entities[i - 1]))
     end
   end
 end
-script.on_event(defines.events.on_player_alt_selected_area, on_player_alt_selected_area)
+script.on_event(defines.events.on_player_selected_area, on_player_selected_area)
