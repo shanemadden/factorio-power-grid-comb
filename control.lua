5344b77b65f1812d344450aa28be201fd9d062f6
Caleb Wishart <caleb.wish@outlook.com>
2024-11-01 19:49:18 +1000

    feat: Update alt-select and remove select feature
    [FFF-379](https://factorio.com/blog/post/fff-379) should have solved
    any issues with ordering, which was the use case for the default select
    
    Updated alt-select to use the new LuaWireConnector interface

--- control.lua
+++ control.lua
@@ -1,74 +1,15 @@
-
-local function on_player_selected_area(event)
-  if event.item == "power-grid-comb" then
-    local distances = {}
-    local create = {}
-    for k, entity in pairs(event.entities) do
-      if entity.type == "electric-pole" and entity.name ~= "ret-pole-wire" then
-        local info = {
-          surface = entity.surface,
-          name = entity.name,
-          unit_number = entity.unit_number,
-          position = entity.position,
-          health = entity.health,
-          force = entity.force,
-          connections = {},
-        }
-        for k, connection in pairs(entity.circuit_connection_definitions) do
-          info.connections[k] = {
-            wire = connection.wire,
-            source_circuit_id = connection.source_circuit_id,
-            target_circuit_id = connection.target_circuit_id,
-            target_unit_number = connection.target_entity.unit_number,
-            target_entity = connection.target_entity,
-          }
-        end
-        local max_wire_distance = entity.prototype.max_wire_distance
-        if create[max_wire_distance] == nil then
-          create[max_wire_distance] = {}
-          table.insert(distances, max_wire_distance)
-        end
-        table.insert(create[max_wire_distance], info)
-        entity.destroy()
-      end
-    end
-    table.sort(distances)
-    local old_new_map = {}
-    for _, distance in ipairs(distances) do
-      for k, newinfo in pairs(create[distance]) do
-        local newpole = newinfo.surface.create_entity({
-          name = newinfo.name,
-          position = newinfo.position,
-          force = newinfo.force,
-        })
-        newpole.health = newinfo.health
-        create[distance][k].newpole = newpole
-        old_new_map[newinfo.unit_number] = newpole
-      end
-    end
-    for _, distance in ipairs(distances) do
-      for _, newinfo in pairs(create[distance]) do
-        for k, connection in pairs(newinfo.connections) do
-          local target
-          if connection.target_entity.valid then
-            target = connection.target_entity
-          else
-            target = old_new_map[connection.target_unit_number]
-          end
-          if target then
-            newinfo.newpole.connect_neighbour({
-              wire = connection.wire,
-              target_entity = target,
-              source_circuit_id = connection.source_circuit_id,
-              target_circuit_id = connection.target_circuit_id,
-            })
-          end
-        end
-      end
+-- Get the copper wire connector from an entity
+-- if it exists, return it, otherwise return nil
+local function get_copper_wire_connector(entity)
+  local connectors = entity.get_wire_connectors(false)
+  for _, connector in pairs(connectors) do
+    if connector.wire_type == defines.wire_type.copper then
+      return connector
     end
   end
+  return nil
 end
-script.on_event(defines.events.on_player_selected_area, on_player_selected_area)
+
 
 local function find_pole_in_direction(entity, direction, distance)
   local selection_box = entity.prototype.selection_box
@@ -76,32 +17,32 @@ local function find_pole_in_direction(entity, direction, distance)
   if direction == defines.direction.north then
     -- top left - top left of current plus distance north
     -- bot right - top right of current
-    area = {{entity.position.x + selection_box.left_top.x,                entity.position.y + selection_box.left_top.y - distance},
-            {entity.position.x + selection_box.right_bottom.x,            entity.position.y + selection_box.left_top.y}}
+    area = { { entity.position.x + selection_box.left_top.x, entity.position.y + selection_box.left_top.y - distance },
+      { entity.position.x + selection_box.right_bottom.x, entity.position.y + selection_box.left_top.y } }
   elseif direction == defines.direction.south then
     -- top left - bot left of current
     -- bot right - bot right of current plus distance south
-    area = {{entity.position.x + selection_box.left_top.x,                entity.position.y + selection_box.right_bottom.y},
-            {entity.position.x + selection_box.right_bottom.x,            entity.position.y + selection_box.right_bottom.y + distance}}
+    area = { { entity.position.x + selection_box.left_top.x, entity.position.y + selection_box.right_bottom.y },
+      { entity.position.x + selection_box.right_bottom.x, entity.position.y + selection_box.right_bottom.y + distance } }
   elseif direction == defines.direction.west then
     -- top left - top left of current plus distance west
     -- bot right - bot left of current
-    area = {{entity.position.x + selection_box.left_top.x - distance,     entity.position.y + selection_box.left_top.y},
-            {entity.position.x + selection_box.left_top.x,                entity.position.y + selection_box.right_bottom.y}}
+    area = { { entity.position.x + selection_box.left_top.x - distance, entity.position.y + selection_box.left_top.y },
+      { entity.position.x + selection_box.left_top.x,            entity.position.y + selection_box.right_bottom.y } }
   elseif direction == defines.direction.east then
     -- top left - top right of current
     -- bot right - bot right of current plus distance east
-    area = {{entity.position.x + selection_box.right_bottom.x,            entity.position.y + selection_box.left_top.y},
-            {entity.position.x + selection_box.right_bottom.x + distance, entity.position.y + selection_box.right_bottom.y}}
+    area = { { entity.position.x + selection_box.right_bottom.x,   entity.position.y + selection_box.left_top.y },
+      { entity.position.x + selection_box.right_bottom.x + distance, entity.position.y + selection_box.right_bottom.y } }
   end
-  local closest_distance
-  local closest
+  local closest_distance = nil
+  local closest = nil
   local entities = entity.surface.find_entities_filtered({
     area = area,
     type = "electric-pole",
     force = entity.force,
   })
-  for _, found_entity in ipairs(entities) do
+  for _, found_entity in pairs(entities) do
     local x_dist = math.abs(entity.position.x - found_entity.position.x)
     local y_dist = math.abs(entity.position.y - found_entity.position.y)
     local found_distance = math.sqrt(x_dist * x_dist + y_dist * y_dist)
@@ -120,13 +61,13 @@ local scan = {
   defines.direction.west,
 }
 
-local function on_player_alt_selected_area(event)
+local function on_player_selected_area(event)
   if event.item == "power-grid-comb" then
     local distances = {}
     local entities = {}
-    for k, entity in pairs(event.entities) do
+    for _, entity in pairs(event.entities) do
       if entity.type == "electric-pole" then
-        local max_wire_distance = entity.prototype.max_wire_distance
+        local max_wire_distance = entity.prototype.get_max_wire_distance(entity.quality)
         if not entities[max_wire_distance] then
           entities[max_wire_distance] = {}
           table.insert(distances, max_wire_distance)
@@ -135,17 +76,24 @@ local function on_player_alt_selected_area(event)
       end
     end
     table.sort(distances)
-    for _, distance in ipairs(distances) do
-      for k, entity in pairs(entities[distance]) do
-        entity.disconnect_neighbour()
-        for _, direction in ipairs(scan) do
-          local pole = find_pole_in_direction(entity, direction, distance)
-          if pole then
-            entity.connect_neighbour(pole)
+    for _, distance in pairs(distances) do
+      for _, entity in pairs(entities[distance]) do
+        local connector = get_copper_wire_connector(entity)
+        if connector ~= nil then
+          connector.disconnect_all()
+        end
+        for _, direction in pairs(scan) do
+          local pole = find_pole_in_direction(entity, direction, distance
+          )
+          if pole ~= nil and connector ~= nil then
+            local pole_connector = get_copper_wire_connector(pole)
+            if pole_connector ~= nil then
+              connector.connect_to(pole_connector)
+            end
           end
         end
       end
     end
   end
 end
-script.on_event(defines.events.on_player_alt_selected_area, on_player_alt_selected_area)
+script.on_event(defines.events.on_player_selected_area, on_player_selected_area)
