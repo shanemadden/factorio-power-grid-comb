9b06f57938991c98391b4ae3f9fe04f6b7d2e2e0
Caleb Wishart <caleb.wish@outlook.com>
2024-11-01 19:49:01 +1000

    fix: update item + shortcut with 2.0 API changes

--- data.lua
+++ data.lua
@@ -3,22 +3,23 @@ local item = {
   name = "power-grid-comb",
   subgroup = "tool",
   order = "z[power-grid-comb]",
-  show_in_library = true,
+
   icons = {
     {
       icon = "__power-grid-comb__/graphics/icons/power-grid-comb.png",
       icon_size = 32,
     }
   },
-  flags = {"only-in-cursor", "spawnable"},
+
+  flags = { "only-in-cursor", "spawnable" },
   stack_size = 1,
-  stackable = false,
-  selection_color = { r = 0.72, g = 0.45, b = 0.2, a = 1 },
-  alt_selection_color = { r = 0.72, g = 0.22, b = 0.1, a = 1 },
-  selection_mode = { "buildable-type", "same-force" },
-  alt_selection_mode = { "buildable-type", "same-force" },
-  selection_cursor_box_type = "entity",
-  alt_selection_cursor_box_type = "entity",
+  hidden = true,
+
+  select = {
+    border_color = { r = 0.72, g = 0.45, b = 0.2, a = 1 },
+    mode = { "buildable-type", "same-force" },
+    cursor_box_type = "entity"
+  },
 }
 
 local shortcut = {
@@ -27,34 +28,10 @@ local shortcut = {
   action = "spawn-item",
   item_to_spawn = "power-grid-comb",
   order = "m[power-grid-comb]",
-  --style = "yellow",
-  icon = {
-    filename = "__power-grid-comb__/graphics/icons/power-grid-comb-x32.png",
-    flags = {
-      "icon"
-    },
-    priority = "extra-high-no-scale",
-    scale = 1,
-    size = 32
-  },
-  small_icon = {
-    filename = "__power-grid-comb__/graphics/icons/power-grid-comb-x24.png",
-    flags = {
-      "icon"
-    },
-    priority = "extra-high-no-scale",
-    scale = 1,
-    size = 24
-  },
-  disabled_small_icon = {
-    filename = "__power-grid-comb__/graphics/icons/power-grid-comb-x24-white.png",
-    flags = {
-      "icon"
-    },
-    priority = "extra-high-no-scale",
-    scale = 1,
-    size = 24
-  },
+  icon_size = 32,
+  icon = "__power-grid-comb__/graphics/icons/power-grid-comb-x32.png",
+  small_icon_size = 24,
+  small_icon = "__power-grid-comb__/graphics/icons/power-grid-comb-x24.png",
 }
 
-data:extend{item, shortcut}
+data:extend { item, shortcut }
