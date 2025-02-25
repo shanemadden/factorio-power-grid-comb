aed0de9b1581a0ac2cf89a4c5e6dcbde2ecf1f77
Caleb Wishart <caleb.wish@outlook.com>
2025-02-26 08:15:09 +1000

    Update for Factorio v2.0+ (#12)
    Co-authored-by: _CodeGreen <mapope8703@gmail.com>

--- control.lua
+++ control.lua
@@ -1,151 +1,149 @@
+local flib_position = require("__flib__.position")
+
+-- Mod Compatibility checks
+local function is_excluded(entity)
+  -- compatibility with Realistic Train Network Mod
+  return entity.name == "ret-pole-wire"
+end
+
+--- @param event EventData.on_player_selected_area|EventData.on_player_alt_selected_area
+--- @param alt_mode? boolean is alt select mode
+local function on_selected_area(event, alt_mode)
+  local unit_numbers = {}
+  local x_axis = {}
+  local y_axis = {}
+  local distances = {}
+  local network_ids = {}
+
+  -- 1. Collect all entity unit numbers and electic network ids before they might change
+  for _, entity in pairs(event.entities) do
+    if is_excluded(entity) then goto continue end
+    unit_numbers[entity.unit_number] = true
+    network_ids[entity.unit_number] = entity.electric_network_id
+    ::continue::
+  end
+
+  -- 2. Disconnect all copper wires and reconnect any required connections
+  for _, entity in pairs(event.entities) do
+    if is_excluded(entity) then goto continue end
+    local to_reconnect = {}
+    local connector = entity.get_wire_connector(defines.wire_connector_id.pole_copper, false)
+    if connector == nil then goto continue end
+
+    for _, connection in pairs(connector.connections) do
+      --  if the connection is not in the selected area, we need to reconnect it
+      -- this also reconnects any connections to non-pole entities like power switches
+      if not unit_numbers[connection.target.owner.unit_number] then
+        to_reconnect[#to_reconnect + 1] = connection
+      end
+    end
+
+    connector.disconnect_all()
+    for _, connection in pairs(to_reconnect) do
+      connector.connect_to(connection.target, false, connection.origin)
+    end
+
+    local position = entity.position
+    local x, y = position.x, position.y
+
+    local x_entities = x_axis[x] or {}
+    x_axis[x] = x_entities
+    x_entities[#x_entities + 1] = entity
+
+    local y_entities = y_axis[y] or {}
+    y_axis[y] = y_entities
+    y_entities[#y_entities + 1] = entity
+
+    -- 2.1 Collect the distances between each entity
+    if not alt_mode then
+      for _, other in pairs(event.entities) do
+        if is_excluded(other) or other == entity then goto next end
+        local distance = flib_position.distance(position, other.position)
+        if distances[entity.unit_number] == nil then
+          distances[entity.unit_number] = {}
+        end
+        local entity_distances = distances[entity.unit_number]
+        entity_distances[#entity_distances + 1] = { entity = other, distance = distance }
+        ::next::
+      end
+    end
+    ::continue::
+  end
+  -- 3. reconnect any poles that are on the same horizontal line
+  for _, entities in pairs(x_axis) do
+    table.sort(entities, function(a, b)
+      return a.position.y < b.position.y
+    end)
+    for i = 2, #entities do
+      if network_ids[entities[i].unit_number] == network_ids[entities[i - 1].unit_number] then
+        local src = entities[i].get_wire_connector(defines.wire_connector_id.pole_copper, true)
+        local dst = entities[i - 1].get_wire_connector(defines.wire_connector_id.pole_copper, true)
+        src.connect_to(dst)
+      end
+    end
+  end
+  -- 4. reconnect any poles that are on the same vertical line
+  for _, entities in pairs(y_axis) do
+    table.sort(entities, function(a, b)
+      return a.position.x < b.position.x
+    end)
+    for i = 2, #entities do
+      if network_ids[entities[i].unit_number] == network_ids[entities[i - 1].unit_number] then
+        local src = entities[i].get_wire_connector(defines.wire_connector_id.pole_copper, true)
+        local dst = entities[i - 1].get_wire_connector(defines.wire_connector_id.pole_copper, true)
+        src.connect_to(dst)
+      end
+    end
+  end
+
+  -- 5. If not in alt mode, connect the closest poles so each network connection is restored
+
+  if not alt_mode then
+    for _, entity in pairs(event.entities) do
+      if is_excluded(entity) then goto continue end
+      local src = entity.get_wire_connector(defines.wire_connector_id.pole_copper, true)
+      if src == nil then goto continue end
+
+      local entity_distances = distances[entity.unit_number]
+      if entity_distances == nil then goto continue end
+
+      table.sort(entity_distances, function(a, b)
+        return a.distance < b.distance
+      end)
+      -- we try and reconnect to the 3 closest poles
+      -- this should handle most cases where connects need to be restored to keep networks connected
+      for _ = 1, 3 do
+        for _, other in pairs(entity_distances) do
+          if other.entity.valid and network_ids[entity.unit_number] == network_ids[other.entity.unit_number] then
+            local dst = other.entity.get_wire_connector(defines.wire_connector_id.pole_copper, true)
+            if src.network_id ~= dst.network_id then
+              src.connect_to(dst)
+              -- only connect to the first closest
+              -- there is no limit on # of connections, only one connection is needed
+              -- if none are found then the pole will remain disconnected
+              goto next
+            end
+          end
+        end
+        -- if none found then no point in continuing
+        break
+        ::next::
+      end
+      ::continue::
+    end
+  end
+end
+
 
 local function on_player_selected_area(event)
-  if event.item == "power-grid-comb" then
-    local distances = {}
-    local create = {}
-    for k, entity in pairs(event.entities) do
-      if entity.type == "electric-pole" and entity.name ~= "ret-pole-wire" then
-        local info = {
-          surface = entity.surface,
-          name = entity.name,
-          unit_number = entity.unit_number,
-          position = entity.position,
-          health = entity.health,
-          force = entity.force,
-          connections = {},
-        }
-        for k, connection in pairs(entity.circuit_connection_definitions) do
-          info.connections[k] = {
-            wire = connection.wire,
-            source_circuit_id = connection.source_circuit_id,
-            target_circuit_id = connection.target_circuit_id,
-            target_unit_number = connection.target_entity.unit_number,
-            target_entity = connection.target_entity,
-          }
-        end
-        local max_wire_distance = entity.prototype.max_wire_distance
-        if create[max_wire_distance] == nil then
-          create[max_wire_distance] = {}
-          table.insert(distances, max_wire_distance)
-        end
-        table.insert(create[max_wire_distance], info)
-        entity.destroy()
-      end
-    end
-    table.sort(distances)
-    local old_new_map = {}
-    for _, distance in ipairs(distances) do
-      for k, newinfo in pairs(create[distance]) do
-        local newpole = newinfo.surface.create_entity({
-          name = newinfo.name,
-          position = newinfo.position,
-          force = newinfo.force,
-        })
-        newpole.health = newinfo.health
-        create[distance][k].newpole = newpole
-        old_new_map[newinfo.unit_number] = newpole
-      end
-    end
-    for _, distance in ipairs(distances) do
-      for _, newinfo in pairs(create[distance]) do
-        for k, connection in pairs(newinfo.connections) do
-          local target
-          if connection.target_entity.valid then
-            target = connection.target_entity
-          else
-            target = old_new_map[connection.target_unit_number]
-          end
-          if target then
-            newinfo.newpole.connect_neighbour({
-              wire = connection.wire,
-              target_entity = target,
-              source_circuit_id = connection.source_circuit_id,
-              target_circuit_id = connection.target_circuit_id,
-            })
-          end
-        end
-      end
-    end
-  end
+  if event.item ~= "power-grid-comb" then return end
+  on_selected_area(event, false)
 end
-script.on_event(defines.events.on_player_selected_area, on_player_selected_area)
-
-local function find_pole_in_direction(entity, direction, distance)
-  local selection_box = entity.prototype.selection_box
-  local area
-  if direction == defines.direction.north then
-    -- top left - top left of current plus distance north
-    -- bot right - top right of current
-    area = {{entity.position.x + selection_box.left_top.x,                entity.position.y + selection_box.left_top.y - distance},
-            {entity.position.x + selection_box.right_bottom.x,            entity.position.y + selection_box.left_top.y}}
-  elseif direction == defines.direction.south then
-    -- top left - bot left of current
-    -- bot right - bot right of current plus distance south
-    area = {{entity.position.x + selection_box.left_top.x,                entity.position.y + selection_box.right_bottom.y},
-            {entity.position.x + selection_box.right_bottom.x,            entity.position.y + selection_box.right_bottom.y + distance}}
-  elseif direction == defines.direction.west then
-    -- top left - top left of current plus distance west
-    -- bot right - bot left of current
-    area = {{entity.position.x + selection_box.left_top.x - distance,     entity.position.y + selection_box.left_top.y},
-            {entity.position.x + selection_box.left_top.x,                entity.position.y + selection_box.right_bottom.y}}
-  elseif direction == defines.direction.east then
-    -- top left - top right of current
-    -- bot right - bot right of current plus distance east
-    area = {{entity.position.x + selection_box.right_bottom.x,            entity.position.y + selection_box.left_top.y},
-            {entity.position.x + selection_box.right_bottom.x + distance, entity.position.y + selection_box.right_bottom.y}}
-  end
-  local closest_distance
-  local closest
-  local entities = entity.surface.find_entities_filtered({
-    area = area,
-    type = "electric-pole",
-    force = entity.force,
-  })
-  for _, found_entity in ipairs(entities) do
-    local x_dist = math.abs(entity.position.x - found_entity.position.x)
-    local y_dist = math.abs(entity.position.y - found_entity.position.y)
-    local found_distance = math.sqrt(x_dist * x_dist + y_dist * y_dist)
-    if found_distance <= distance and (closest_distance == nil or found_distance < closest_distance) then
-      closest = found_entity
-      closest_distance = found_distance
-    end
-  end
-  return closest
-end
-
-local scan = {
-  defines.direction.north,
-  defines.direction.south,
-  defines.direction.east,
-  defines.direction.west,
-}
 
 local function on_player_alt_selected_area(event)
-  if event.item == "power-grid-comb" then
-    local distances = {}
-    local entities = {}
-    for k, entity in pairs(event.entities) do
-      if entity.type == "electric-pole" then
-        local max_wire_distance = entity.prototype.max_wire_distance
-        if not entities[max_wire_distance] then
-          entities[max_wire_distance] = {}
-          table.insert(distances, max_wire_distance)
-        end
-        table.insert(entities[max_wire_distance], entity)
-      end
-    end
-    table.sort(distances)
-    for _, distance in ipairs(distances) do
-      for k, entity in pairs(entities[distance]) do
-        entity.disconnect_neighbour()
-        for _, direction in ipairs(scan) do
-          local pole = find_pole_in_direction(entity, direction, distance)
-          if pole then
-            entity.connect_neighbour(pole)
-          end
-        end
-      end
-    end
-  end
+  if event.item ~= "power-grid-comb" then return end
+  on_selected_area(event, true)
 end
+
+script.on_event(defines.events.on_player_selected_area, on_player_selected_area)
 script.on_event(defines.events.on_player_alt_selected_area, on_player_alt_selected_area)
